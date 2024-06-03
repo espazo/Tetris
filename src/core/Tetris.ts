2d53228808015ebbfe1d21621ee7112909fad56f
xiaoruib <xiaoruib@yonyou.com>
2024-06-03 10:49:43 +0800

    feature: tetris producer module

---
+++ src/core/Tetris.ts
@@ -0,0 +1,80 @@
+import {Point, Shape} from "./types";
+import {getRandom} from "./util";
+import {SquareGroup} from "./SquareGroup";
+
+export const TShape: Shape = [
+    {x: -1, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 1},
+    {x: 0, y: -1},
+];
+
+export const LShape: Shape = [
+    {x: -2, y: 0},
+    {x: -1, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: -1},
+];
+
+export const LMirrorShape: Shape = [
+    {x: 2, y: 0},
+    {x: 1, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: -1},
+];
+
+export const SShape: Shape = [
+    {x: 0, y: 0},
+    {x: 1, y: 0},
+    {x: 0, y: 1},
+    {x: -1, y: 1},
+];
+
+export const SMirrorShape: Shape = [
+    {x: 0, y: 0},
+    {x: -1, y: 0},
+    {x: 0, y: 1},
+    {x: 1, y: 1},
+];
+
+export const SquareShape: Shape = [
+    {x: 0, y: 0},
+    {x: 1, y: 0},
+    {x: 0, y: 1},
+    {x: 1, y: 1},
+];
+
+export const LineShape: Shape = [
+    {x: -1, y: 0},
+    {x: 0, y: 0},
+    {x: 0, y: 1},
+    {x: 0, y: 2},
+];
+
+export const shapes = [
+    TShape,
+    LShape,
+    LMirrorShape,
+    SShape,
+    SMirrorShape,
+    SquareShape,
+    LineShape,
+];
+
+export const colors = [
+    'red',
+    'white',
+    'green',
+    'blue',
+    'orange',
+];
+
+export function createTetris(centerPoint: Point) {
+    let index = getRandom(0, shapes.length);
+    const shape = shapes[index];
+
+    index = getRandom(0, colors.length);
+    const color = colors[index];
+
+    return new SquareGroup(shape, centerPoint, color);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,37 @@
 import $ from 'jquery';
 import {SquarePageViewer} from "./core/viewer/SquarePageViewer";
 import {SquareGroup} from "./core/SquareGroup";
+import {createTetris, SMirrorShape} from "./core/Tetris";
 
-const group = new SquareGroup([
-    {x: 0, y: -1},
-    {x: -1, y: 0},
-    {x: 0, y: 0},
-    {x: 0, y: 1},
-], {x: 1, y: 1}, 'red');
-group.squares.forEach(sq => {
+const tetris = createTetris({x: 3, y: 2});
+tetris.squares.forEach(sq => {
     sq.viewer = new SquarePageViewer(sq, $('#root'));
 });
 
 $('#up').click(() => {
-    group.centerPoint = {
-        x: group.centerPoint.x,
-        y: group.centerPoint.y - 1,
+    tetris.centerPoint = {
+        x: tetris.centerPoint.x,
+        y: tetris.centerPoint.y - 1,
     };
 });
 
 $('#down').click(() => {
-    group.centerPoint = {
-        x: group.centerPoint.x,
-        y: group.centerPoint.y + 1,
+    tetris.centerPoint = {
+        x: tetris.centerPoint.x,
+        y: tetris.centerPoint.y + 1,
     };
 });
 
 $('#left').click(() => {
-    group.centerPoint = {
-        x: group.centerPoint.x - 1,
-        y: group.centerPoint.y,
+    tetris.centerPoint = {
+        x: tetris.centerPoint.x - 1,
+        y: tetris.centerPoint.y,
     };
 });
 
 $('#right').click(() => {
-    group.centerPoint = {
-        x: group.centerPoint.x + 1,
-        y: group.centerPoint.y,
+    tetris.centerPoint = {
+        x: tetris.centerPoint.x + 1,
+        y: tetris.centerPoint.y,
     };
 });
